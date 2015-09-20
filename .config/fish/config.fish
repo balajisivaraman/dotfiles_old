7b92123e11b32bb30dfdd0cb0b0b73724f1608ae
Balaji Sivaraman <balaji@balajisivaraman.com>
2015-09-20 16:58:31 +0530

    Add basic fish shell configuration, sans OMF.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,14 +1,6 @@
-# Path to your oh-my-fish.
-set fish_path $HOME/.oh-my-fish
+set -x FISH_CONFIG_PATH $HOME/.fish
 
-# Path to your custom folder (default path is ~/.oh-my-fish/custom)
-#set fish_custom $HOME/dotfiles/oh-my-fish
-
-# Load oh-my-fish configuration.
-. $fish_path/oh-my-fish.fish
-
-Theme "robbyrussell"
-Plugin "archlinux"
-Plugin "osx"
-Plugin "ssh"
-Plugin "balaji"
+source $FISH_CONFIG_PATH/aliases.fish
+source $FISH_CONFIG_PATH/balaji.fish
+source $FISH_CONFIG_PATH/nix.load
+source $FISH_CONFIG_PATH/prompt.load
---
+++ .fish/balaji.fish
@@ -0,0 +1,34 @@
+if uname -a | grep Darwin > /dev/null
+
+  set GHC_DOT_APP /Applications/ghc-7.10.2.app
+
+  if set -q  GHC_DOT_APP
+    set -x PATH $HOME/.cabal/bin $GHC_DOT_APP/Contents/bin $PATH
+  end
+
+  set -x PATH $HOME/Projects/haskell/stack-ide/.stack-work/install/x86_64-osx/nightly-2015-08-06/7.10.2/bin $PATH
+
+  set -x GRADLE_HOME ~/Software/coding/java/gradle-2.4/
+  set -x PATH /usr/local/Cellar/git/2.5.1/bin $PATH
+  set -x PATH $GRADLE_HOME/bin $PATH
+
+  set -x JAVA_HOME /Library/Java/JavaVirtualMachines/jdk1.8.0_60.jdk/Contents/Home
+  set -x JAVA_8_HOME /Library/Java/JavaVirtualMachines/jdk1.8.0_60.jdk/Contents/Home
+
+end
+
+if uname -a | grep GNU/Linux > /dev/null
+
+    set -x PATH $HOME/.stack/programs/x86_64-linux/ghc-7.10.2/bin $PATH
+    set -x PATH $HOME/Projects/elm-lang/elm-bin/.cabal-sandbox/bin $PATH
+    set -x PATH $HOME/.gem/ruby/2.2.0/bin $PATH
+
+end
+
+set -x PATH ~/bin/ $PATH
+set -x PATH $HOME/.local/bin/ $PATH
+
+set p $HOME/Projects/
+set twp $HOME/TWProjects/
+set omf $HOME/.oh-my-fish/
+set emacs $HOME/.emacs.d/
