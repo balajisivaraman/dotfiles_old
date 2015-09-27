de345b9cf38749755e47c3a281e9ad904d52ce10
Balaji Sivaraman <balaji@balajisivaraman.com>
2015-09-27 21:08:59 +0530

    Add git aliases & stuff taken from Gary Bernhardt

--- .fish/aliases.fish
+++ .fish/aliases.fish
@@ -2,17 +2,7 @@
 alias l 'ls -alrt'
 
 #Git
-alias ggpush 'git push origin master'
-alias ggpull 'git pull origin master'
-alias gco 'git checkout'
-alias gcl 'git clone'
-alias gcmsg 'git commit -m'
-alias gst 'git status'
-alias gd 'git diff'
-alias gf 'git fetch'
-alias gm 'git merge'
-alias ga 'git add'
-alias gl 'git log --graph --pretty=oneline'
+alias g git
 
 #Arch
 alias pacin 'sudo pacman -S $argv'
