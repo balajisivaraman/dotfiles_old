81e98e813537dece4f14c268997f7bfee0c97b0b
Balaji Sivaraman <balaji@balajisivaraman.com>
2016-05-26 22:57:32 +0530

    Nix is not needed for now

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,5 @@ set -x FISH_CONFIG_PATH $HOME/.fish
 
 source $FISH_CONFIG_PATH/aliases.fish
 source $FISH_CONFIG_PATH/balaji.fish
-source $FISH_CONFIG_PATH/nix.load
 source $FISH_CONFIG_PATH/prompt.load
 rvm default
