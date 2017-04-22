63126ef30bf558ae3fbe9923e76f3e0072f31d7a
Balaji Sivaraman <balaji@balajisivaraman.com>
2017-04-22 21:24:38 +0530

    [gpg] Set GPG TTY to ensure sane behaviour

--- .config/fish/conf.d/init.fish
+++ .config/fish/conf.d/init.fish
@@ -1,6 +1,7 @@
 source ~/.config/fish/conf.d/aliases.fish
 source ~/.config/fish/conf.d/prompt.load
 
+set -x GPG_TTY (tty)
 set -x PATH ~/bin/ $PATH
 set -x PATH $HOME/.local/bin/ $PATH
 set -x PATH $HOME/.bin/ $PATH
