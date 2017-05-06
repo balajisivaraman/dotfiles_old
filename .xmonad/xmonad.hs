1155f865b5b088244b6e67e3e2720401fdb8b76b
Balaji Sivaraman <balaji@balajisivaraman.com>
2017-05-07 01:02:38 +0530

    [nix] Execute tiling WM startup script on XMonad startup

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -4,8 +4,12 @@ import XMonad
 myTerminal = "urxvt"
 myModMask = mod4Mask
 
+myStartupHook = do
+    spawn "~/bin/wm/init-tilingwm"
+
 main :: IO()
 main = xmonad $ defaultConfig
     { terminal = myTerminal
     , modMask = myModMask
+    , startupHook = myStartupHook
     }
