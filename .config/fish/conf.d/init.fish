6e2c9408e824da4b0ba922132c427390ec559c56
Balaji Sivaraman <balaji@balajisivaraman.com>
2017-09-15 19:13:47 +0530

    Set RUST_SRC_PATH for racer to work properly

--- .config/fish/conf.d/init.fish
+++ .config/fish/conf.d/init.fish
@@ -2,6 +2,7 @@ source ~/.config/fish/conf.d/aliases.fish
 source ~/.config/fish/conf.d/prompt.load
 
 set -x GPG_TTY (tty)
+set -x RUST_SRC_PATH $HOME/Projects/rust/rust-lang/src
 set -x PATH ~/bin/ $PATH
 set -x PATH $HOME/.local/bin/ $PATH
 set -x PATH $HOME/.bin/ $PATH
