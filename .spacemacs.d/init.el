c1f711eb7e3d1a9339c36807c4a1f3c582acea62
Balaji Sivaraman <balaji@balajisivaraman.com>
2017-09-15 12:20:13 +0530

    Pull in some custom org mode configurations from my Emacs config

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -350,6 +350,8 @@ configuration.
 Put your configuration code here, except for variables that should be set
 before packages are loaded."
   (require 'init-pragmata-pro)
+  (use-package init-org
+    :after org)
   )
 
 ;; Do not write anything past this comment. This is where Emacs will
