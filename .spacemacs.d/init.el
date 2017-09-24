cf93b745f4d4fa3ba9b4a2e97287fa7a53b81e74
Balaji Sivaraman <balaji@balajisivaraman.com>
2017-09-24 22:23:15 +0530

    [spacemacs] Add shell scripts layer

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -53,6 +53,7 @@ This function should only modify configuration layer settings."
      (shell :variables
             shell-default-height 30
             shell-default-position 'bottom)
+     shell-scripts
      spell-checking
      syntax-checking
      version-control
