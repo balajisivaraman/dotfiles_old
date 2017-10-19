7ea22df030fe2f996a19853daf7c9a6787eed328
Balaji Sivaraman <balaji@balajisivaraman.com>
2017-10-19 16:23:59 +0530

    [fish] Remove a whole bunch of aliases I don't use anymore

--- .config/fish/conf.d/aliases.fish
+++ .config/fish/conf.d/aliases.fish
@@ -3,14 +3,6 @@ alias l 'ls -alhFrt'
 
 #Git
 alias g git
-alias st 'git status'
-
-#Arch
-alias pacin 'sudo pacman -S $argv'
-alias pacrem 'sudo pacman -Rns $argv'
-alias y 'yaourt'
-alias ys 'yaourt -S $argv'
-alias yr 'yaourt -R $argv'
 
 #Ruby
 alias rc 'rails console'
@@ -18,12 +10,3 @@ alias rs 'rails server'
 
 #Pam
 alias lb './build_scripts/local_build.sh'
-
-#Convenience
-alias twp 'cd $HOME/TWProjects/'
-alias pam 'cd $HOME/TWProjects/pam'
-alias p 'cd $HOME/Projects/'
-alias emacsd 'cd $HOME/.emacs.d'
-
-#NVim
-alias v 'nvim'
