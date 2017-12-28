9a8a8f8d7a0bf154c10afa6ddf6976b9d715c46a
Balaji Sivaraman <balaji@balajisivaraman.com>
2017-12-28 18:30:07 +0530

    Arch has autojump installed in a different location sadly

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,4 +4,4 @@ if status --is-login
     end
 end
 
-[ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
+[ -f /usr/share/autojump/autojump.fish ]; and source /usr/share/autojump/autojump.fish
