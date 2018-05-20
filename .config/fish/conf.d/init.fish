c00a1548e424ebb67faede89ed03556a9554271e
Balaji Sivaraman <balaji@balajisivaraman.com>
2018-05-20 17:51:54 +0530

    [fish] Add FZF binaries to PATH

--- .config/fish/conf.d/init.fish
+++ .config/fish/conf.d/init.fish
@@ -5,5 +5,5 @@ set -x GPG_TTY (tty)
 set -x RUST_SRC_PATH $HOME/Projects/rust/rust-lang/src
 set -x PATH ~/bin/ $PATH
 set -x PATH $HOME/.local/bin/ $PATH
-set -x PATH $HOME/.bin/ $PATH
+set -x PATH $HOME/.fzf/bin $PATH
 set -x PATH $HOME/.cargo/bin $PATH
