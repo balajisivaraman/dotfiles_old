2cdc37c0d8c6987a8a42147d6a4e40612de6d6d7
Balaji Sivaraman <balaji@balajisivaraman.com>
2018-12-08 16:56:16 +0530

    [fish] Remove no longer needs paths from PATH

--- .config/fish/conf.d/init.fish
+++ .config/fish/conf.d/init.fish
@@ -2,8 +2,6 @@ source ~/.config/fish/conf.d/aliases.fish
 
 set -x GPG_TTY (tty)
 set -x RUST_SRC_PATH $HOME/Projects/rust/rust-lang/src
-set -x PATH ~/bin/ $PATH
-set -x PATH $HOME/.local/bin/ $PATH
 set -x PATH $HOME/.fzf/bin $PATH
 set -x PATH $HOME/.cargo/bin $PATH
 set -x PATH /usr/share/bcc/tools $PATH
