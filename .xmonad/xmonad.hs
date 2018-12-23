a1b6f41bf131ebdc9629891b532fd799df18726c
Balaji Sivaraman <balaji@balajisivaraman.com>
2018-12-23 16:10:11 +0530

    [xmonad] Setup alacritty and kill bindings

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,8 +1,16 @@
 import XMonad
-import XMonad.Util.EZConfig
+import XMonad.Util.EZConfig (additionalKeys)
+
+myTerminal :: String
+myTerminal = "alacritty"
+
+myBrowser :: String
+myBrowser = "/opt/firefox-nightly/firefox"
 
 main = xmonad $ def
      { modMask = mod4Mask
      } `additionalKeys`
-     [ ((mod4Mask, xK_b), spawn "/opt/firefox-nightly/firefox")
+     [ ((mod4Mask, xK_b), spawn myBrowser),
+       ((mod4Mask, xK_Return), spawn myTerminal),
+       ((mod4Mask, xK_BackSpace), kill)
      ]
