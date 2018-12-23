f739060808873a482a11668e06bd638f51fd6aea
Balaji Sivaraman <balaji@balajisivaraman.com>
2018-12-23 16:52:55 +0530

    [xmonad] Revamp as per new Prime config

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,5 +1,8 @@
-import XMonad
-import XMonad.Util.EZConfig (additionalKeys)
+{-# LANGUAGE RebindableSyntax #-}
+import XMonad.Config.Prime
+
+import Data.Map (Map)
+import XMonad.Util.EZConfig (additionalKeys, mkKeymap)
 
 myTerminal :: String
 myTerminal = "alacritty"
@@ -7,10 +10,14 @@ myTerminal = "alacritty"
 myBrowser :: String
 myBrowser = "/opt/firefox-nightly/firefox"
 
-main = xmonad $ def
-     { modMask = mod4Mask
-     } `additionalKeys`
-     [ ((mod4Mask, xK_b), spawn myBrowser),
-       ((mod4Mask, xK_Return), spawn myTerminal),
-       ((mod4Mask, xK_BackSpace), kill)
-     ]
+myKeys :: XConfig l -> Map (KeyMask, KeySym) (X ())
+myKeys c = mkKeymap c $
+    [
+      ("M-b", spawn myBrowser)
+    , ("M-<Return>", spawn myTerminal)
+    , ("M-<Backspace>", kill)
+    ]
+
+main = xmonad $ do
+    let keys = myKeys
+    modMask =: mod4Mask
