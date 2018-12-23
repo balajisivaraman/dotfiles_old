7dfb96cb7fd6fd701827c74a848451ab01f04141
Balaji Sivaraman <balaji@balajisivaraman.com>
2018-12-23 18:07:50 +0530

    [fish] Remove bcc tools for now

--- .config/fish/conf.d/init.fish
+++ .config/fish/conf.d/init.fish
@@ -2,4 +2,3 @@ set -x GPG_TTY (tty)
 set -x RUST_SRC_PATH $HOME/Projects/rust/rust-lang/src
 set -x PATH $HOME/.fzf/bin $PATH
 set -x PATH $HOME/.cargo/bin $PATH
-set -x PATH /usr/share/bcc/tools $PATH
