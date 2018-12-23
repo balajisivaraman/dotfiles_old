af3c98fbfa677a5cddfae55fd7d963cfe3ed7ca5
Balaji Sivaraman <balaji@balajisivaraman.com>
2018-12-23 20:03:17 +0530

    [polybar] Add polybar and launch with WM

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -3,6 +3,7 @@ import XMonad.Config.Desktop
 import XMonad.Hooks.EwmhDesktops (ewmh)
 import XMonad.Layout.Fullscreen (fullscreenSupport)
 import XMonad.Util.EZConfig (additionalKeys)
+import XMonad.Util.SpawnOnce (spawnOnce)
 
 myTerminal :: String
 myTerminal = "alacritty"
@@ -10,10 +11,17 @@ myTerminal = "alacritty"
 myBrowser :: String
 myBrowser = "/opt/firefox-nightly/firefox"
 
+myStartupHook = do
+    spawnOnce "$HOME/bin/wm/init-polybar"
+
+myWorkspaces = ["web","dev","misc"]
+
 main = xmonad
      $ fullscreenSupport
      $ ewmh desktopConfig
      { modMask = mod4Mask
+     , startupHook = myStartupHook <+> startupHook desktopConfig
+     , workspaces = myWorkspaces
      } `additionalKeys`
      [ ((mod4Mask, xK_b), spawn myBrowser),
        ((mod4Mask, xK_Return), spawn myTerminal),
---
+++ bin/wm/init-polybar
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+m=$(xrandr --query | grep " connected" | cut -d" " -f1)
+cmd=(env "MONITOR=$m" polybar --reload main)
+
+if [[ $# -gt 0 ]] && [[ $1 = "block" ]]; then
+	exec "${cmd[@]}"
+else
+	"${cmd[@]}" &
+fi
