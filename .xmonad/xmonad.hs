354ad38a92bff5a65f0c919cab65a85bf3d680d7
Balaji Sivaraman <balaji@balajisivaraman.com>
2018-12-23 22:00:13 +0530

    [xmonad] Cleanup and add comments to myself

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -8,33 +8,34 @@ import XMonad.Layout.NoBorders (noBorders)
 import XMonad.Layout.Spacing
 import XMonad.Util.EZConfig (additionalKeys)
 import XMonad.Util.Run (spawnPipe, hPutStrLn)
-import XMonad.Util.SpawnOnce (spawnOnce)
 
-myTerminal :: String
 myTerminal = "alacritty"
-
-myBrowser :: String
 myBrowser = "/opt/firefox-nightly/firefox"
 
-myWorkspaces = ["web","dev","misc"]
-
 mySpacing = spacingRaw True (Border 0 10 10 10) True (Border 10 10 10 10) True
-myLayoutHook = noBorders $ mySpacing $ avoidStruts $ layoutHook def
+myLayoutHook = noBorders -- Don't want borders
+    $ mySpacing -- Setup 10 spacing as above
+    $ avoidStruts -- Needed to ensure XMobar stays on top
+    $ layoutHook def
+
 myStatusBar = "xmobar -x1"
+-- Below is needed to ensure content is output to XMobar StdIn
 myLogHook h = do
     dynamicLogWithPP $ def
         { ppOutput = hPutStrLn h
         }
 
+myWorkspaces = ["web","dev","misc"]
+
 main = do
     xmproc <- spawnPipe myStatusBar
     xmonad
-       $ fullscreenSupport
-       $ docks
-       $ ewmh
+       $ fullscreenSupport -- Sets up everything for fullscreen support
+       $ docks -- Needed to ensure stuff like XMobar stays on top
+       $ ewmh -- Advertises EWMH support in XMonad
        $ myConfig xmproc
 
-myConfig p = desktopConfig
+myConfig p = desktopConfig -- Sets up basic desktop related configuration
     { modMask = mod4Mask
     , layoutHook = myLayoutHook
     , logHook = myLogHook p
