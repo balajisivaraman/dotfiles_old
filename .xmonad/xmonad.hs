769fe617d3f3c3818c281576fda041c48d461663
Balaji Sivaraman <balaji@balajisivaraman.com>
2018-12-23 21:51:47 +0530

    [xmonad] Delete polybar. Setup xmobar, stalonetray and wallpaper

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,11 +1,13 @@
 import XMonad
 import XMonad.Config.Desktop
+import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.EwmhDesktops (ewmh)
 import XMonad.Hooks.ManageDocks (avoidStruts, docks)
 import XMonad.Layout.Fullscreen (fullscreenSupport)
 import XMonad.Layout.NoBorders (noBorders)
 import XMonad.Layout.Spacing
 import XMonad.Util.EZConfig (additionalKeys)
+import XMonad.Util.Run (spawnPipe, hPutStrLn)
 import XMonad.Util.SpawnOnce (spawnOnce)
 
 myTerminal :: String
@@ -14,24 +16,31 @@ myTerminal = "alacritty"
 myBrowser :: String
 myBrowser = "/opt/firefox-nightly/firefox"
 
-myStartupHook = do
-    spawnOnce "$HOME/bin/wm/init-polybar"
-
 myWorkspaces = ["web","dev","misc"]
 
 mySpacing = spacingRaw True (Border 0 10 10 10) True (Border 10 10 10 10) True
 myLayoutHook = noBorders $ mySpacing $ avoidStruts $ layoutHook def
+myStatusBar = "xmobar -x1"
+myLogHook h = do
+    dynamicLogWithPP $ def
+        { ppOutput = hPutStrLn h
+        }
 
-main = xmonad
-     $ fullscreenSupport
-     $ docks
-     $ ewmh desktopConfig
-     { modMask = mod4Mask
-     , layoutHook = myLayoutHook
-     , startupHook = myStartupHook <+> startupHook desktopConfig
-     , workspaces = myWorkspaces
-     } `additionalKeys`
-     [ ((mod4Mask, xK_b), spawn myBrowser),
-       ((mod4Mask, xK_Return), spawn myTerminal),
-       ((mod4Mask, xK_BackSpace), kill)
-     ]
+main = do
+    xmproc <- spawnPipe myStatusBar
+    xmonad
+       $ fullscreenSupport
+       $ docks
+       $ ewmh
+       $ myConfig xmproc
+
+myConfig p = desktopConfig
+    { modMask = mod4Mask
+    , layoutHook = myLayoutHook
+    , logHook = myLogHook p
+    , workspaces = myWorkspaces
+    } `additionalKeys`
+    [ ((mod4Mask, xK_b), spawn myBrowser),
+      ((mod4Mask, xK_Return), spawn myTerminal),
+      ((mod4Mask, xK_BackSpace), kill)
+    ]
