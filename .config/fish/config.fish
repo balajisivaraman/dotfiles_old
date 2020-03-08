a3d7d35b8ecb9981b829a8f59adb2b99795b8ee0
Balaji Sivaraman <balaji@balajisivaraman.com>
2020-03-08 16:07:11 +0530

    fish: remove unnecessary startx command

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,3 @@
-if status --is-login
-    if test -z "$DISPLAY" -a $XDG_VTNR = 1
-        exec startx -- -keeptty
-    end
-end
-
 [ -f /usr/share/autojump/autojump.fish ]; and source /usr/share/autojump/autojump.fish
 
 starship init fish | source
