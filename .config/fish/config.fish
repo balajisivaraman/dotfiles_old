f5b4b86a3492b19738021f89a72c8a0530e4110f
Balaji Sivaraman <balaji@balajisivaraman.com>
2020-04-16 18:06:32 +0530

    fish: revert back to bobthefish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,5 @@
 [ -f /usr/share/autojump/autojump.fish ]; and source /usr/share/autojump/autojump.fish
 
-starship init fish | source
+set -g theme_display_user yes
+set -g theme_display_hostname yes
+set -g theme_color_scheme dracula
