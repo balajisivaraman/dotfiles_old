2e0b24933e039b9b9ef2daf7308bf667ee50ae53
Balaji Sivaraman <balaji@balajisivaraman.com>
2020-04-17 18:28:13 +0530

    fish: add go dirs to PATH and set Go env vars

--- .config/fish/conf.d/init.fish
+++ .config/fish/conf.d/init.fish
@@ -2,3 +2,6 @@ set -x GPG_TTY (tty)
 set -x RUST_SRC_PATH $HOME/Projects/rust/rust-lang/src
 set -x PATH $HOME/.fzf/bin $PATH
 set -x PATH $HOME/.cargo/bin $PATH
+set -x PATH $HOME/go/bin $PATH
+set -x GOPATH $HOME/go
+set -x GOBIN $HOME/go/bin
