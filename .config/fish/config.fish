8fec69743655f0c1774d618d9ecc4589a860ba3f
Balaji Sivaraman <balaji@balajisivaraman.com>
2020-04-29 11:13:05 +0530

    fish: go back to starship

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,3 @@
 [ -f /usr/share/autojump/autojump.fish ]; and source /usr/share/autojump/autojump.fish
 
-set -g theme_display_user yes
-set -g theme_display_hostname yes
-set -g theme_color_scheme dracula
+starship init fish | source
