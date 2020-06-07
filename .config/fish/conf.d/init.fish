5d23a28e38a6a7bdace36a119dab915e4bcbdd14
Balaji Sivaraman <balaji@balajisivaraman.com>
2020-06-07 15:25:58 +0530

    temp

--- .config/fish/conf.d/init.fish
+++ .config/fish/conf.d/init.fish
@@ -3,5 +3,19 @@ set -x RUST_SRC_PATH $HOME/Projects/rust/rust-lang/src
 set -x PATH $HOME/.fzf/bin $PATH
 set -x PATH $HOME/.cargo/bin $PATH
 set -x PATH $HOME/go/bin $PATH
+set -x PATH $HOME/bin $PATH
 set -x GOPATH $HOME/go
 set -x GOBIN $HOME/go/bin
+
+set -x NEXUS_USERNAME sc-0102-ins_dev
+set -x NEXUS_PASSWORD generali
+set -x GITLAB_PRIVATE_TOKEN Koem8deMRuPYasYXW4q7
+set -x MAESTRO_API_CALLER_ROOT ~/code/generali/sin-maestro-api-caller
+
+function setOpenJdk8
+    set -Ux JAVA_HOME (/usr/libexec/java_home -v 1.8)
+end
+
+function setOpenJdk11
+    set -Ux JAVA_HOME (/usr/libexec/java_home -v 11)
+end
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,5 @@
-[ -f /usr/share/autojump/autojump.fish ]; and source /usr/share/autojump/autojump.fish
+[ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
+
+source ~/.config/fish/conf.d/forgit.plugin.fish
 
 starship init fish | source
