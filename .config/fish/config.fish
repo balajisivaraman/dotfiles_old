f893545a5074399544df1a1ee3669497ba131749
Balaji Sivaraman <balaji@balajisivaraman.com>
2020-07-30 23:22:41 +0530

    fish: startup gnome keyring daemon as per Arch wiki guidelines

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,7 @@
+if test -n "$DESKTOP_SESSION"
+    set (gnome-keyring-daemon --start | string split "=")
+end
+
 [ -f /usr/share/autojump/autojump.fish ]; and source /usr/share/autojump/autojump.fish
 
 starship init fish | source
