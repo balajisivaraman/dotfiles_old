eb1207b23d9e42dedebbc64e55525057658616ed
Balaji Sivaraman <balaji@balajisivaraman.com>
2021-07-07 19:11:53 +0530

    fix: put fzf colors configuration in a single line

--- .config/fish/conf.d/init.fish
+++ .config/fish/conf.d/init.fish
@@ -5,8 +5,4 @@ set -x PATH $HOME/.cargo/bin $PATH
 set -x PATH $HOME/go/bin $PATH
 set -x GOPATH $HOME/go
 set -x GOBIN $HOME/go/bin
-set -x FZF_DEFAULT_OPTS '--height 40% --layout=reverse --border
---color=fg:#4a484d,bg:#ffffff,hl:#1d3ccf
---color=fg+:#5e4b4f,bg+:#efefef,hl+:#185570
---color=info:#714700,prompt:#88267a,pointer:#700dc9
---color=marker:#005d26,spinner:#700dc9,header:#2d45b0'
+set -x FZF_DEFAULT_OPTS '--height 40% --layout=reverse --border --color=fg:#4a484d,bg:#ffffff,hl:#1d3ccf,fg+:#5e4b4f,bg+:#efefef,hl+:#185570,info:#714700,prompt:#88267a,pointer:#700dc9,marker:#005d26,spinner:#700dc9,header:#2d45b0'
