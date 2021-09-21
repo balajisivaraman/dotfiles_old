75bd4fcd65dc88e44259a94fb3cfafcf9627fc89
Balaji Sivaraman <balaji@balajisivaraman.com>
2021-09-21 22:13:27 +0530

    fish: add ~/.local/bin to PATH

--- fish/.config/fish/conf.d/init.fish
+++ fish/.config/fish/conf.d/init.fish
@@ -2,6 +2,7 @@ set -x GPG_TTY (tty)
 set -x RUST_SRC_PATH $HOME/Projects/rust/rust-lang/src
 set -x PATH $HOME/.fzf/bin $PATH
 set -x PATH $HOME/.cargo/bin $PATH
+set -x PATH $HOME/.local/bin $PATH
 set -x PATH $HOME/go/bin $PATH
 set -x GOPATH $HOME/go
 set -x GOBIN $HOME/go/bin
