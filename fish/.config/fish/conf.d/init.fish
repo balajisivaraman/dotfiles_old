4732f6e65bd17ec46c3c8a0ee7bef58e052463e0
Balaji Sivaraman <balaji@balajisivaraman.com>
2021-10-10 22:16:17 +0530

    config: unset RUST_SRC_PATH in Fish

--- fish/.config/fish/conf.d/init.fish
+++ fish/.config/fish/conf.d/init.fish
@@ -1,5 +1,4 @@
 set -x GPG_TTY (tty)
-set -x RUST_SRC_PATH $HOME/Projects/rust/rust-lang/src
 set -x PATH $HOME/.fzf/bin $PATH
 set -x PATH $HOME/.cargo/bin $PATH
 set -x PATH $HOME/.local/bin $PATH
