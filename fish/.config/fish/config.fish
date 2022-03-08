9da3cada45bad85ff36130c4ae60e61cccc5396c
Balaji Sivaraman <balaji@balajisivaraman.com>
2022-03-08 21:38:00 +0530

    fix: remove explicit startup of Gnome Keyring Daemon
    We no longer need this as we're on PopOS where this is already setup
    for us.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,3 @@
-if test -n "$DESKTOP_SESSION"
-    set (gnome-keyring-daemon --start | string split "=")
-end
-
 [ -f /usr/share/autojump/autojump.fish ]; and source /usr/share/autojump/autojump.fish
 
 starship init fish | source
