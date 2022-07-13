31958f01d0fd83fd3a887bd269c0ebd8145742ef
Balaji Sivaraman <balaji@balajisivaraman.com>
2022-07-13 22:54:15 +0530

    feat: load autojump for different path on OSX

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,4 +1,8 @@
-[ -f /usr/share/autojump/autojump.fish ]; and source /usr/share/autojump/autojump.fish
+if [ $hostname = "korriban.local" ]
+    [ -f $(brew --prefix)/share/autojump/autojump.fish ]; and source $(brew --prefix)/share/autojump/autojump.fish
+else
+    [ -f /usr/share/autojump/autojump.fish ]; and source /usr/share/autojump/autojump.fish
+end
 
 set -x EDITOR 'emacsclient -c'
 
