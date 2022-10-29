5ea061ebcd4d47532945e3740d4f4e0161c4f446
Balaji Sivaraman <balaji@balajisivaraman.com>
2022-10-29 19:25:16 +0530

    fix: issue with Modus Vivendi FZF theme config

--- fish/.config/fish/conf.d/init.fish
+++ fish/.config/fish/conf.d/init.fish
@@ -5,8 +5,4 @@ set -x PATH $HOME/.local/bin $PATH
 set -x PATH $HOME/go/bin $PATH
 set -x GOPATH $HOME/go
 set -x GOBIN $HOME/go/bin
-set -x FZF_DEFAULT_OPTS '--height 40% --layout=reverse --border
- --color=fg:#ffffff,bg:#121212,hl:#90c4ed
- --color=fg+:#ffffff,bg+:#3c3c3c,hl+:#00d3d0
- --color=info:#c0c530,prompt:#f78fe7,pointer:#9f80ff
- --color=marker:#70b900,spinner:#feacd0,header:#6ae4b9'
+set -x FZF_DEFAULT_OPTS '--height 40% --layout=reverse --border --color=fg:#ffffff,bg:#121212,hl:#90c4ed --color=fg+:#ffffff,bg+:#3c3c3c,hl+:#00d3d0 --color=info:#c0c530,prompt:#f78fe7,pointer:#9f80ff --color=marker:#70b900,spinner:#feacd0,header:#6ae4b9'
